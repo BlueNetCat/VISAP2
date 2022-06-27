35b2f9c76dcfd4cdf30efa5ffcd8207ee64ddde4
Gerard Llorach <gerard.llorach@gmail.com>
2022-06-27 18:23:01 +0200

    funding sentence shortened

--- About.vue
+++ About.vue
@@ -50,7 +50,7 @@
     "Sentence3": "A team made up of thirteen technicians specialized in marine biological sciences and in geolocation and information support computer tools, coordinated and advised by researchers from the Department of Renewable Resources of the Barcelona Institute of Marine Sciences, have been the material authors of the scientific follow-up that has been aimed at maritime fisheries in both their professional and recreational aspects.",
     "Sentence4": "In October 2018, the monitoring program for the species of greatest fishing interest was launched, where one of the key elements is the collection of data and their structuring so that they can be analyzed and visualized.",
     "SentenceContact": "For inquiries about the application, please contact Gerard Llorach (gllorach at bluenetcat.eu). For inquiries about the data, please contact Jordi Ribera (jribera at icm.csic.es).",
-    "SentenceFunding": "The application has been developed by the data service of the Xarxa Marítima de Catalunya (BlueNetCat), which is financed by the Generalitat de Catalunya and by the European Regional Development Fund (ERDF)."
+    "SentenceFunding": "Xarxa Marítima de Catalunya (BlueNetCat) is financed by the Generalitat de Catalunya and by the European Regional Development Fund (ERDF)."
   },
   "es": {
     "About": "Acerca de nosotros",
@@ -63,7 +63,7 @@
     "Sentence3": "Un equipo formado por trece técnicos especializados en ciencias biológicas del ámbito marino y en herramientas informáticas de geolocalización y de apoyo a la información, coordinados y asesorados por investigadores del Departamento de Recursos Renovables del Instituto de Ciencias del Mar de Barcelona, han sido los autores materiales del seguimiento científico que ha ido dirigido a las pesqueras marítimas en sus vertientes tanto profesional como recreativa.",
     "Sentence4": "En Octubre de 2018 se puso en marcha el programa de seguimiento de las especies de mayor interés pesquero, donde uno de los elementos clave es la recopilación de datos y su estructuración para poder ser analizadas y visualizadas.",
     "SentenceContact": "Si necesita más información sobre la aplicación, puede contactar con Gerard Llorach (gllorach at bluenetcat.eu). Si necesita más información sobre los datos, puede contactar con Jordi Ribera (jribera at icm.csic.es).",
-    "SentenceFunding": "La aplicación ha sido desarrollada por el servicio de datos de la Xarxa Marítima de Catalunya (BlueNetCat), que está financiada por la Generalitat de Catalunya y el Fondo Europeo de Desarrollo Regional (FEDER)."
+    "SentenceFunding": "La Xarxa Marítima de Catalunya (BlueNetCat) está financiada por la Generalitat de Catalunya y el Fondo Europeo de Desarrollo Regional (FEDER)."
   },
   "ca": {
     "About": "Sobre nosaltres",
@@ -76,7 +76,7 @@
     "Sentence3": "Un equip format per tretze tècnics especialitzats en ciències biològiques de l'àmbit marí i en eines informàtiques de geolocalització i de suport a la informació, coordinats i assessorats per investigadors del Departament de Recursos Renovables de l'Institut de Ciències del Mar de Barcelona, han estat els autors materials del seguiment científic que ha anat dirigit a les pesqueres marítimes en les vessants tant professional com recreativa.",
     "Sentence4": "A l'Octubre de 2018 es va posar en marxa el programa de seguiment de les especies de major interès pesquer, on un dels elements clau és la recopilació de dades i la seva estructuració per a poder ser analitzades i visualitzades.",
     "SentenceContact": "En cas de voler més informació sobre l'aplicació, podeu contactar amb en Gerard Llorach (gllorach at bluenetcat.eu). En cas de voler més informació sobre les dades, podeu contactar amb en Jordi Ribera (jribera at icm.csic.es).",
-    "SentenceFunding": "L'aplicació ha estat desenvolupada per el servei de dades de la Xarxa Marítima de Catalunya (BlueNetCat), que està finançada per la Generalitat de Catalunya i el Fons Europeu de Desenvolupament Regional (FEDER)."
+    "SentenceFunding": "La Xarxa Marítima de Catalunya (BlueNetCat) està finançada per la Generalitat de Catalunya i el Fons Europeu de Desenvolupament Regional (FEDER)."
   }
 }
 </i18n>
